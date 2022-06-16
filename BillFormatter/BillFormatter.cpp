c4e794053563b75dafeaa77597bbc7002e4b7aef
Thomas Strunz <thomas.salchinger@zoho.com>
2022-06-16 01:56:25 +0000

    Remove example code

--- BillFormatter/BillFormatter.cpp
+++ BillFormatter/BillFormatter.cpp
@@ -13,69 +13,7 @@
 
 #include "wkhtmltox/pdf.h"
 
-//#include "mycomplex.h"
-//class mycomplex;
-
-#define _MAX_SIZE 123
-constexpr int MAX_SIZE = 123;
-#define MAX(lhs,rhs){lhs>rhs?lhs:rhs}
-
-//typedef enum {
-//    apple = 0,
-//    orange,
-//    banana
-//} fruit_t;
-
-using fruit = enum { apple, banana };
-
-//typedef unsigned char byte_t;
-using byte_t = unsigned char;
-
 using namespace std;
-
-//void func(char val[2][4])
-//void func(const char* val[])
-void func(const char* val[])
-{
-    int i = 123;
-    int j = 456;
-    int x = ( i > j ? i : j ), y = 987;
-    std::cout << "func:\n";
-    std::cout << val[0] << std::endl;
-    std::cout << val[1] << std::endl;
-}
-
-void func(char* val[])
-{
-    int i = 123;
-    int j = 456;
-    int x = ( i > j ? i : j ), y = 987;
-    std::cout << "func:\n";
-    std::cout << val[0] << std::endl;
-    std::cout << val[1] << std::endl;
-    val[0][0] = 'A';
-}
-
-void func_modern(const array<const char*, 2>& val)
-{
-    std::cout << "func_modern:\n";
-    std::cout << val[0] << std::endl;
-    std::cout << val[1] << std::endl;
-}
-
-string* func_string(const vector<string>& val)
-{
-    //string other;
-    string* str = new string("Good morning");
-    //string str = "Good morning";
-    *str = "Good morning, Dr. Chandra!";
-    //std::cout << "String length: " << str->size() << std::endl;
-    std::cout << "func_string:\n";
-    std::cout << val[0] << std::endl;
-    std::cout << val[1] << std::endl;
-    //delete str;
-    return str;
-}
 
 void print_usage()
 {
@@ -88,61 +26,8 @@ void print_usage()
     cout << endl;
 }
 
-//template<typename T>
-//auto get_value(T t)
-//{
-//    if constexpr (std::is_pointer_v<T>)
-//        return *t; // deduces return type to int for T = int*
-//    else
-//        return t;  // deduces return type to int for T = int
-//}
-
-//ostream& bold(ostream& os) {
-//    //return os << '\033' << '[';
-//    return os << "<strong>";
-//}
-
 int main(int argc, char* argv[])
 {
-    //cout << "regular " << bold << "boldface" << endl;
-    //cout << hex << 255 << " " << 254 << " " << hex << 253 << endl;
-    //cout << "Pi: " << acos(-1.) << endl;
-    //cout << "Pi: " << setprecision(3) << acos(-1.) << acos(-1.) << endl;
-    //cout << "Pi: " << setw(8) << setprecision(3) << acos(-1.) << endl;
-    //cout << "Pi: " << setfill('_') << setw(8) << setprecision(3) << acos(-1.) << endl;
-    //cout << "Pi: " << setfill('_') << setprecision(3) << acos(-1.) << endl;
-    //return 0;
-
-    //mycomplex* c1 = new mycomplex;
-    //cout << "Before: " << c1 << endl;
-    //*c1 = 123;
-    //cout << "After: " << c1 << endl;
-    //delete c1;
-
-    //mycomplex c2;
-    //c2 = 23;
-
-    //mycomplex<float> c1;
-    //mycomplex<int> c0;
-    //cin >> c1 >> c0;
-    //cout << "The complex object is ";
-    //cout << c1 << c0;
-
-    //istringstream sin("67 89");
-    //sin >> c1;
-    //cout << "The complex object is ";
-    //cout << c1;
-
-    //return 0;
-
-    //// if constexpr example usage
-    //int i = 123;
-    //int j = 456;
-    //int* p = &j;
-    //cout << "Value of int: " << get_value(i) << endl;
-    //cout << "Value of pointer: " << get_value(p) << endl;
-    //cout << endl;
-
     vector<string> inputDocs;
 
     // Parsing the command line arguments and options
@@ -171,60 +56,6 @@ int main(int argc, char* argv[])
         cout << doc << endl;
     }
     cout << endl;
-
-    /*
-    argv[0]: "...exe"
-    argv[1]: "/d"
-    argv[2]: "*rc"
-
-    
-    "Hello World\0"
-    [H][e][l][l][o][ ]....
-     ^
-     |
-     char* pstr;
-
-    "/d\0"
-    {
-    [/][d][\0],
-    [*][r][c][\0]
-    }
-
-    char arr[5][10];
-    [/][d][\0][][],[*][r][c][\0][],....
-
-
-    */
-
-    //std::cout << "Hello World!\n";
-    //std::cout << "Arg Count: " << argc << std::endl;
-
-    //const char* val[] = 
-    //{
-    //    "/d",
-    //    "*rc"
-    //};
-    //char val1[2][8] = 
-    //{
-    //    "/d",
-    //    "*rc"
-    //};
-
-    //// C-style array of pointers
-    //char* ptrs[2];
-    //ptrs[0] = val1[0];
-    //ptrs[1] = val1[1];
-    //ptrs[0]++;
-    //cout << "val1: " << val1[0] << endl;
-    //func(ptrs);
-    //cout << "val1: " << val1[0] << endl;
-
-    //// std::array of pointers
-    //func_modern({ val[0] + 1, val[1] });
-
-    //// Vector of std::strings
-    //auto* str = func_string({ val[0] + 1, val[1] });
-    //delete str;
 
 }
 
