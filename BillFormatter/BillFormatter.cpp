b97c3466dc039157168342e7d328e81c34d15b4a
Thomas Strunz <thomas.salchinger@zoho.com>
2022-06-17 16:44:26 +0000

    Convert HTML documents using lib wkhtmltox

--- BillFormatter/BillFormatter.cpp
+++ BillFormatter/BillFormatter.cpp
@@ -26,9 +26,34 @@ void print_usage()
     cout << endl;
 }
 
+/* Print out loading progress information */
+void progress_changed(wkhtmltopdf_converter* c, int p) {
+    printf("%3d%%\r", p);
+    fflush(stdout);
+}
+
+/* Print loading phase information */
+void phase_changed(wkhtmltopdf_converter* c) {
+    int phase = wkhtmltopdf_current_phase(c);
+    printf("%s\n", wkhtmltopdf_phase_description(c, phase));
+}
+
+/* Print a message to stderr when an error occurs */
+void error(wkhtmltopdf_converter* c, const char* msg) {
+    fprintf(stderr, "Error: %s\n", msg);
+}
+
+/* Print a message to stderr when a warning is issued */
+void warning(wkhtmltopdf_converter* c, const char* msg) {
+    fprintf(stderr, "Warning: %s\n", msg);
+}
+
 int main(int argc, char* argv[])
 {
     vector<string> inputDocs;
+
+	wkhtmltopdf_init(1);
+    cout << "WKHtmlToX version " << wkhtmltopdf_version() << endl;
 
     // Parsing the command line arguments and options
     for (int i = 1; i < argc; i++)
@@ -49,14 +74,47 @@ int main(int argc, char* argv[])
         }
     }
 
+	wkhtmltopdf_global_settings *global_settings = wkhtmltopdf_create_global_settings();
+
     // Print the list of arguments
     cout << "Input documents:\n";
     for (auto doc : inputDocs)
     {
-        cout << doc << endl;
+		cout << doc << endl;
+
+		auto out = doc + ".pdf";
+		wkhtmltopdf_set_global_setting(global_settings, "out", out.data());
+		wkhtmltopdf_converter* converter = wkhtmltopdf_create_converter(global_settings);
+		wkhtmltopdf_object_settings* settings = wkhtmltopdf_create_object_settings();
+		wkhtmltopdf_set_object_setting(settings, "page", doc.data());
+		wkhtmltopdf_add_object(converter, settings, nullptr);
+
+		/* Call the progress_changed function when progress changes */
+		wkhtmltopdf_set_progress_changed_callback(converter, progress_changed);
+
+		/* Call the phase _changed function when the phase changes */
+		wkhtmltopdf_set_phase_changed_callback(converter, phase_changed);
+
+		/* Call the error function when an error occurs */
+		wkhtmltopdf_set_error_callback(converter, error);
+
+		/* Call the warning function when a warning is issued */
+		wkhtmltopdf_set_warning_callback(converter, warning);
+
+		if (!wkhtmltopdf_convert(converter))
+		{
+			cout << "Failed to convert " << doc << endl;
+		}
+
+		wkhtmltopdf_destroy_converter(converter);
+		wkhtmltopdf_destroy_object_settings(settings);
     }
     cout << endl;
 
+    wkhtmltopdf_destroy_global_settings(global_settings);
+    wkhtmltopdf_deinit();
+
+    return 0;
 }
 
 // Topics:
@@ -77,6 +135,7 @@ int main(int argc, char* argv[])
 // - virtual
 // - public
 // - explicit
+// - DLL example project
 
 // - <fstream>, <ofstream>: open, truncate
 //   https://en.cppreference.com/w/cpp/io/basic_ofstream
