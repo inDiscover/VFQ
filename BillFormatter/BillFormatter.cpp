8994bb71ad52af4d1bfac586d2a8c58dc288d9ea
Thomas Strunz <thomas.salchinger@zoho.com>
2022-06-24 17:46:29 +0000

    Begin deploying jobs to child processes

--- BillFormatter/BillFormatter.cpp
+++ BillFormatter/BillFormatter.cpp
@@ -2,7 +2,10 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include <array>
+#include <ostream>
+#include <sstream>
 #include <vector>
 #include <deque>
 #include <string>
@@ -21,52 +24,35 @@
 #endif
 
 #include "wkhtmltox/pdf.h"
+#include "zmq.hpp"
+#include "zmq_addon.hpp"
 
 #include "HtmlConverter.h"
+#include "worker.h"
 
 using namespace std;
 using namespace std::chrono_literals;
 
+bool g_is_worker = false;
+std::vector<worker> g_workers;
 deque<html_converter> converters;
-std::vector<std::thread> g_threads;
 bool g_terminate_pool = false;
-std::mutex g_queue_mtx;
-std::condition_variable g_cv_pool;
+//std::mutex g_queue_mtx;
+//std::condition_variable g_cv_pool;
 size_t g_done_count = 0u;
 size_t g_job_count = 0u;
 size_t g_shutdown_count = 0u;
 size_t max_threads = 1;
-std::mutex g_done_mtx;
-std::condition_variable g_cv_done;
-std::mutex g_shutdown_mtx;
-std::condition_variable g_cv_shutdown;
-const auto TIMEOUT_ALL_JOBS_FINISHED(5000ms);
-const auto TIMEOUT_SHUTDOWN_WORKERS(5000ms);
+//std::mutex g_done_mtx;
+//std::condition_variable g_cv_done;
+//std::mutex g_shutdown_mtx;
+//std::condition_variable g_cv_shutdown;
+//const auto TIMEOUT_ALL_JOBS_FINISHED(5000ms);
+//const auto TIMEOUT_SHUTDOWN_WORKERS(5000ms);
 
-namespace
-{
-    /* Print out loading progress information */
-    void progress_changed(wkhtmltopdf_converter* c, int p) {
-        printf("%3d%%\r", p);
-        fflush(stdout);
-    }
-
-    /* Print loading phase information */
-    void phase_changed(wkhtmltopdf_converter* c) {
-        int phase = wkhtmltopdf_current_phase(c);
-        printf("%s\n", wkhtmltopdf_phase_description(c, phase));
-    }
-
-    /* Print a message to stderr when an error occurs */
-    void error(wkhtmltopdf_converter* c, const char* msg) {
-        fprintf(stderr, "Error: %s\n", msg);
-    }
-
-    /* Print a message to stderr when a warning is issued */
-    void warning(wkhtmltopdf_converter* c, const char* msg) {
-        fprintf(stderr, "Warning: %s\n", msg);
-    }
-}
+static const std::string MSG_JOB_REQ = "JOB_REQ";
+static const std::string MSG_WORKER_READY = "WORKER_READY";
+static const std::string MSG_WORKER_TERM = "WORKER_TERM";
 
 void print_usage()
 {
@@ -81,19 +67,60 @@ void print_usage()
 
 bool wait_for_shutdown()
 {
-    std::unique_lock<std::mutex> lock(g_shutdown_mtx);
-    return g_cv_shutdown.wait_for(lock, TIMEOUT_SHUTDOWN_WORKERS, [] { return g_shutdown_count == max_threads; });
+    //std::unique_lock<std::mutex> lock(g_shutdown_mtx);
+    //return g_cv_shutdown.wait_for(lock, TIMEOUT_SHUTDOWN_WORKERS, [] { return g_shutdown_count == max_threads; });
+    std::this_thread::sleep_for(5s);
+    return true;
 }
 
-void shutdown_pool()
+void signal_shutdown_pool(zmq::socket_t& broker)
+{
+    // Signal shutdown of all workers
+    for (auto i=0; i<max_threads; ++i)
+    {
+        //  Next message gives us least recently used worker
+        zmq::message_t identity_msg;
+        auto rc = broker.recv(identity_msg, zmq::recv_flags::none);
+        if (!rc)
+        {
+            std::cerr << "Failed to receive workers identity" << std::endl;
+            break;
+        }
+
+        std::array<zmq::const_buffer, 3> req_msgs =
+        {
+            zmq::buffer(identity_msg.to_string()),
+            zmq::str_buffer(""),          // envelop delimiter
+            zmq::buffer(MSG_WORKER_TERM)  // shutdown request
+        };
+        try
+        {
+            if (!zmq::send_multipart(broker, req_msgs))
+            {
+                std::cerr << "Failed to send WORKER_TERM to worker " << identity_msg.to_string() << std::endl;
+            }
+            else
+            {
+                std::cout << "Sent WORKER_TERM to " << identity_msg.to_string() << std::endl;
+            }
+        }
+        catch (const zmq::error_t& err)
+        {
+            std::cerr << "Failed to send WORKER_TERM to worker " << identity_msg.to_string() << ": " << err.what() << std::endl;
+        }
+    }
+}
+
+void shutdown_pool(zmq::socket_t& broker)
 {
     {
-        std::unique_lock<std::mutex> lock(g_queue_mtx);
-        g_terminate_pool = true; // use this flag in condition.wait
-        std::cout << "Signal all threads to shutdown" << std::endl;
+        //std::unique_lock<std::mutex> lock(g_queue_mtx);
+        g_terminate_pool = true;
+        signal_shutdown_pool(broker);
+        std::cout << "Signaled to shutdown worker" << std::endl;
     }
 
-    g_cv_pool.notify_all(); // wake up all threads.
+    //g_cv_pool.notify_all(); // wake up all threads.
 
     if (!wait_for_shutdown())
     {
@@ -102,32 +129,51 @@ void shutdown_pool()
         std::abort();
     }
 
-    // Join all threads.
-    for (std::thread& th : g_threads)
-    {
-        if (th.joinable())
-        {
-            //th.join();
-            th.detach();
-        }
-    }
+    //// Join all threads.
+    //for (std::thread& th : g_threads)
+    //{
+    //    if (th.joinable())
+    //    {
+    //        //th.join();
+    //        th.detach();
+    //    }
+    //}
 
     converters.clear();
 }
 
 bool wait_for_all_jobs()
 {
-    std::unique_lock<std::mutex> lock(g_done_mtx);
-    return g_cv_done.wait_for(lock, TIMEOUT_ALL_JOBS_FINISHED, [] { return g_done_count == g_job_count; });
+    //std::unique_lock<std::mutex> lock(g_done_mtx);
+    //return g_cv_done.wait_for(lock, TIMEOUT_ALL_JOBS_FINISHED, [] { return g_done_count == g_job_count; });
+    std::this_thread::sleep_for(60s);
+    return true;
 }
 
-void signal_job_done()
+void signal_worker_ready(zmq::socket_t& worker, const std::string& worker_id)
 {
     {
-        std::unique_lock<std::mutex> lock(g_done_mtx);
-        ++g_done_count;
+        //std::unique_lock<std::mutex> lock(g_done_mtx);
+        //++g_done_count;
     }
-    g_cv_done.notify_all();
+    //g_cv_done.notify_all();
+
+    std::array<zmq::const_buffer, 2> req_msgs =
+    {
+        zmq::str_buffer(""),    // envelop delimiter
+        zmq::buffer(MSG_WORKER_READY)
+    };
+    try
+    {
+        if (!zmq::send_multipart(worker, req_msgs))
+        {
+            std::cerr << "Worker " << worker_id << " failed to send WORKER_READY." << std::endl;
+        }
+    }
+    catch (const zmq::error_t& err)
+    {
+        std::cerr << "Worker " << worker_id << " failed to send WORKER_READY." << err.what() << std::endl;
+    }
 }
 
 void add_job(const std::string& doc)
@@ -135,46 +181,112 @@ void add_job(const std::string& doc)
     std::cout << "Add job for " << doc << std::endl;
 
     {
-        std::unique_lock<std::mutex> lock(g_queue_mtx);
+        //std::unique_lock<std::mutex> lock(g_queue_mtx);
         converters.emplace_back(doc);
         ++g_job_count;
     }
 
-    g_cv_pool.notify_one();
+    //g_cv_pool.notify_one();
 }
 
-static unsigned int __stdcall thread_main()
+static unsigned int worker_main(zmq::context_t& context, int backend_port)
 {
+    zmq::socket_t worker(context, zmq::socket_type::dealer);
+    std::ostringstream sout;
+    sout << std::setw(5) << std::setfill('0') << std::hex << std::this_thread::get_id();
+    auto worker_id = sout.str();
+    std::cout << "Worker " << worker_id << " connects to port " << backend_port << std::endl;
+    //worker.setsockopt(ZMQ_IDENTITY, worker_id.data(), worker_id.size());
+    zmq_setsockopt(worker.handle(), ZMQ_IDENTITY, worker_id.data(), worker_id.size());
+    auto endpoint = std::string("tcp://localhost:") + std::to_string(backend_port);
+    worker.connect(endpoint.data());
+
     for (;;)
     {
-            std::unique_lock<std::mutex> lock(g_queue_mtx);
-            g_cv_pool.wait(lock, [] {
-                return !converters.empty() || g_terminate_pool;
-            });
+        // Unlock this worker
+        //std::unique_lock<std::mutex> lock(g_queue_mtx);
+        //g_cv_pool.wait(lock, [] {
+        //    return !converters.empty() || g_terminate_pool;
+        //});
 
-            if (g_terminate_pool)
-            {
-                std::cout << "Terminate pool thread" << std::endl;
-                std::unique_lock<std::mutex> lock(g_shutdown_mtx);
-                ++g_shutdown_count;
-                g_cv_shutdown.notify_one();
-                return 0;
-            }
-            auto job = converters.front();
-            converters.pop_front();
-            lock.unlock();
+        std::cout << "Worker " << worker_id << " signals WORKER_READY" << std::endl;
+        signal_worker_ready(worker, worker_id);
 
-            std::cout << "Processing job for " << job.get_doc() << std::endl;
-            if (!job.convert())
+        std::cout << "Worker " << worker_id << " waiting for job requests..." << std::endl;
+
+        std::vector<zmq::message_t> recv_msgs;
+        const auto rc = zmq::recv_multipart(
+            worker, std::back_inserter(recv_msgs));
+        if (!rc)
+        {
+            std::cerr << "Worker " << worker_id << " failed to receive multi-part job request" << std::endl;
+            break;
+        }
+        std::cout << "Got " << *rc
+                  << " messages" << std::endl;
+        zmq::message_t& identity_msg = recv_msgs[0];
+        zmq::message_t& delimiter_msg = recv_msgs[1];
+        zmq::message_t& workload_msg = recv_msgs[2];
+
+        //zmq::message_t identity_msg;
+        //auto rc = worker.recv(identity_msg, zmq::recv_flags::none); // worker identity
+        //std::cout << "Worker " << worker_id << " received identity " << std::quoted(identity_msg.to_string()) << " [" << rc.value() << "]" << std::endl;
+        //zmq::message_t workload_msg;
+        //rc = worker.recv(workload_msg, zmq::recv_flags::none); // skip envelop delimiter
+        //std::cout << "Worker " << worker_id << " received delimiter " << std::quoted(workload_msg.to_string()) << " [" << rc.value() << "]" << std::endl;
+        //rc = worker.recv(workload_msg, zmq::recv_flags::none);
+        //std::cout << "Worker " << worker_id << " received workload " << std::quoted(workload_msg.to_string()) << " [" << rc.value() << "]" << std::endl;
+        auto workload = workload_msg.to_string();
+
+        std::cout << "Worker " << worker_id << " received request: " << workload << std::endl;
+        if (MSG_WORKER_TERM == workload)
+        {
+            g_terminate_pool = true;
+        }
+        else if (workload.find(MSG_JOB_REQ) != std::string::npos)
+        {
+            std::istringstream sin(workload);
+            sin >> workload >> workload;
+            if (sin)
             {
-                std::cerr << "Failed to convert " << job.get_doc() << ". Error=" << job.get_html_error_code() << std::endl;
+                add_job(workload);
             }
             else
             {
-                const unsigned char* doc_buffer = nullptr;
-                std::cout << "Converted document size " << job.get_output_buffer(&doc_buffer) << std::endl;
+                std::cerr << "Worker " << worker_id << "failed to process workload: " << workload_msg << std::endl;
             }
-            signal_job_done();
+        }
+
+        //if (g_terminate_pool || converters.empty())
+        if (g_terminate_pool)
+        {
+            std::cout << "Terminate pool thread. " << g_done_count << " jobs done." << std::endl;
+            //std::unique_lock<std::mutex> lock(g_shutdown_mtx);
+            ++g_shutdown_count;
+            //g_cv_shutdown.notify_one();
+            return 0;
+        }
+
+        if (converters.empty())
+        {
+            continue;
+        }
+
+        auto job = converters.front();
+        converters.pop_front();
+        //lock.unlock();
+
+        std::cout << "Processing job for " << job.get_doc() << std::endl;
+        if (!job.convert())
+        {
+            std::cerr << "Failed to convert " << job.get_doc() << ". Error=" << job.get_html_error_code() << std::endl;
+        }
+        else
+        {
+            const unsigned char* doc_buffer = nullptr;
+            std::cout << "Converted document size " << job.get_output_buffer(&doc_buffer) << std::endl;
+            ++g_done_count;
+        }
     }
 
     return 1;
@@ -186,6 +298,9 @@ int main(int argc, char* argv[])
 
     wkhtmltopdf_init(1);
     cout << "WKHtmlToX version " << wkhtmltopdf_version() << endl;
+
+    zmq::context_t context(1);
+    auto backend_port = 8877;
 
     // Parsing the command line arguments and options
     for (int i = 1; i < argc; i++)
@@ -199,6 +314,24 @@ int main(int argc, char* argv[])
         if (arg == "-h")
         {
             print_usage();
+        }
+        else if (arg == "-worker")
+        {
+            //__debugbreak();
+            //DebugBreak();
+            g_is_worker = true;
+        }
+        else if (arg.find("-backend=") != std::string::npos)
+        {
+            std::istringstream sin {arg};
+            char c;
+            while (sin >> c && c != '=') {}
+            sin >> backend_port;
+            if (!sin)
+            {
+                std::cerr << "Argument -backend is malformed: " << sin.str() << std::endl;
+                std::cerr << "port: " << backend_port << std::endl;
+            }
         }
         else
         {
@@ -219,60 +352,105 @@ int main(int argc, char* argv[])
     {
         cout << doc << endl;
 
-        add_job(doc);
-
-        //auto out = doc + ".pdf";
-        //auto* global_settings = wkhtmltopdf_create_global_settings();
-        //wkhtmltopdf_set_global_setting(global_settings, "out", out.data());
-        //auto* converter = wkhtmltopdf_create_converter(global_settings);
-        //auto* settings = wkhtmltopdf_create_object_settings();
-        //wkhtmltopdf_set_object_setting(settings, "page", doc.data());
-        //wkhtmltopdf_add_object(converter, settings, nullptr);
-
-        ///* Call the progress_changed function when progress changes */
-        //wkhtmltopdf_set_progress_changed_callback(converter, progress_changed);
-
-        ///* Call the phase _changed function when the phase changes */
-        //wkhtmltopdf_set_phase_changed_callback(converter, phase_changed);
-
-        ///* Call the error function when an error occurs */
-        //wkhtmltopdf_set_error_callback(converter, error);
-
-        ///* Call the warning function when a warning is issued */
-        //wkhtmltopdf_set_warning_callback(converter, warning);
-        //wkhtmltopdf_convert(converter);
+        if (g_is_worker)
+        {
+            add_job(doc);
+        }
     }
+    cout << "Backend port: " << backend_port << endl;
     cout << endl;
 
-    //while (0 < converters.size())
-    //{
-    //        auto job = converters.front();
-    //        converters.pop_front();
-
-    //        std::cout << "Converting document " << job.get_doc() << std::endl;
-    //        if (!job.convert())
-    //        {
-    //            std::cerr << "Failed to convert " << job.get_doc() << std::endl;
-    //        }
-    //}
-
-
-    for (auto i = 0u; i < max_threads; ++i)
+    if (g_is_worker)
     {
-        g_threads.emplace_back(thread_main);
-    }
-
-    //std::this_thread::sleep_for(2000ms);
-    if (wait_for_all_jobs())
-    {
-        std::cout << "All " << g_job_count << " jobs have finished" << std::endl;
+        worker_main(context, backend_port);
     }
     else
     {
-        std::cerr << "Timeout occurred while waiting for jobs to finish" << std::endl;
+        // Distribute all given input documents to the workers
+        //
+        zmq::socket_t broker(context, zmq::socket_type::router);
+        std::vector<std::vector<std::string>> doc_matrix(max_threads);
+        //for (auto i = 0u; i < inputDocs.size(); ++i)
+        //{
+            //auto worker_idx = i % max_threads;
+            //doc_matrix[worker_idx].push_back(inputDocs[i]);
+        //}
+
+        auto endpoint = std::string("tcp://*:") + std::to_string(backend_port);
+        broker.bind(endpoint.data());
+
+        for (auto i = 0u; i < max_threads; ++i)
+        {
+            g_workers.emplace_back(argv[0], backend_port, doc_matrix[i]);
+        }
+
+        for (auto& doc : inputDocs)
+        {
+            std::cout << "Deploy job for " << std::quoted(doc) << std::endl;
+
+            //  Next message gives us least recently used worker
+            zmq::message_t identity_msg;
+            auto rc = broker.recv(identity_msg, zmq::recv_flags::none);
+            if (!rc)
+            {
+                std::cerr << "Failed to receive worker's identity" << std::endl;
+                break;
+            }
+            std::cout << "Received worker's identity " << std::quoted(identity_msg.to_string()) << std::endl;
+
+            // Receive the workers response
+            zmq::message_t reply_msg;
+            rc = broker.recv(reply_msg, zmq::recv_flags::none); // skip envelop delimiter
+            rc = broker.recv(reply_msg, zmq::recv_flags::none);
+            if (!rc)
+            {
+                std::cerr << "Failed to receive worker's reply" << std::endl;
+                break;
+            }
+            std::cout << "Received worker's reply " << std::quoted(reply_msg.to_string()) << std::endl;
+
+            auto reply = reply_msg.to_string();
+            if (MSG_WORKER_READY == reply)
+            {
+                ++g_done_count;
+            }
+
+            // Deploy more work
+            std::ostringstream sout;
+            sout << MSG_JOB_REQ << ' ' << doc;
+            std::array<zmq::const_buffer, 3> req_msgs =
+            {
+                zmq::buffer(identity_msg.to_string()),
+                zmq::str_buffer(""),     // envelop delimiter
+                zmq::buffer(sout.str())  // workload
+            };
+            try
+            {
+                std::cout << "Sending multi-part JOB_REQ {" << identity_msg.to_string() << ", " << sout.str() << "}" << std::endl;
+                if (!zmq::send_multipart(broker, req_msgs))
+                {
+                    std::cerr << "Failed to send JOB_REQ." << std::endl;
+                }
+            }
+            catch (const zmq::error_t& err)
+            {
+                std::cerr << "Failed to send JOB_REQ." << err.what() << std::endl;
+            }
+        }
+
+        shutdown_pool(broker);
     }
 
-    shutdown_pool();
+    //std::this_thread::sleep_for(2000ms);
+    //if (wait_for_all_jobs())
+    //{
+        //std::cout << "All " << g_job_count << " jobs have finished" << std::endl;
+    //}
+    //else
+    //{
+        //std::cerr << "Timeout occurred while waiting for jobs to finish" << std::endl;
+    //}
+
     wkhtmltopdf_deinit();
 
     return 0;
---
+++ BillFormatter/worker.cpp
@@ -0,0 +1,122 @@
+#include "worker.h"
+#include <iostream>
+#include <array>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+namespace
+{
+	void create_child_process(const char* cmd_line, PROCESS_INFORMATION* proc_info)
+	{
+#if _MSC_VER >= 1910
+		STARTUPINFOA start_info;
+		BOOL success = FALSE;
+
+		// Set up members of the PROCESS_INFORMATION structure.
+
+		ZeroMemory(proc_info, sizeof(PROCESS_INFORMATION));
+
+		// Set up members of the STARTUPINFO structure.
+		// This structure specifies the STDIN and STDOUT handles for redirection.
+
+		ZeroMemory(&start_info, sizeof(STARTUPINFO));
+		start_info.cb = sizeof(STARTUPINFO);
+		//start_info.hStdError = g_hChildStd_OUT_Wr;
+		//start_info.hStdOutput = g_hChildStd_OUT_Wr;
+		//start_info.hStdInput = g_hChildStd_IN_Rd;
+		//start_info.dwFlags |= STARTF_USESTDHANDLES;
+
+		// Create the child process.
+
+		success = CreateProcessA(NULL,
+			(LPSTR)cmd_line,// command line
+			NULL,          // process security attributes
+			NULL,          // primary thread security attributes
+			//TRUE,          // handles are inherited
+			FALSE,          // handles are inherited
+			0,             // creation flags
+			NULL,          // use parent's environment
+			NULL,          // use parent's current directory
+			&start_info,   // STARTUPINFO pointer
+			proc_info);    // receives PROCESS_INFORMATION
+
+		 // If an error occurs, exit the application.
+		if (!success)
+		{
+			std::array<char, 500> buffer;
+			::FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, ::GetLastError(), 0, buffer.data(), (DWORD)buffer.size(), nullptr);
+			std::cerr << "Failed to spawn child process: " << buffer.data() << std::endl;
+		}
+		else
+		{
+			// Close handles to the child process and its primary thread.
+			// Some applications might keep these handles to monitor the status
+			// of the child process, for example.
+
+			//CloseHandle(proc_info->hProcess);
+			//CloseHandle(proc_info->hThread);
+
+			// Close handles to the stdin and stdout pipes no longer needed by the child process.
+			// If they are not explicitly closed, there is no way to recognize that the child process has ended.
+
+			//CloseHandle(g_hChildStd_OUT_Wr);
+			//CloseHandle(g_hChildStd_IN_Rd);
+		}
+#endif
+	}
+}
+
+worker::worker()
+	: proc_info(nullptr)
+{
+}
+
+worker::worker(const worker& other)
+	: proc_info(other.proc_info)
+{
+	const_cast<worker&>(other).proc_info = nullptr;
+}
+
+worker::worker(worker&& other) noexcept
+	: proc_info(std::exchange(other.proc_info, nullptr))
+{
+}
+
+worker::worker(const std::string& prog_name, int backend_port, const std::vector<std::string>& docs)
+{
+	auto cmd_line = prog_name + " -worker " + " -backend=" + std::to_string(backend_port) + " ";
+	std::for_each(docs.begin(), docs.end(), [&cmd_line](auto& el) { cmd_line += el + " "; });
+	proc_info = new PROCESS_INFORMATION;
+	create_child_process(cmd_line.data(), proc_info);
+}
+
+worker::~worker()
+{
+	auto exit_code = 0ul;
+	if (proc_info)
+	{
+		auto success = GetExitCodeProcess(proc_info->hProcess, &exit_code);
+		if (success && exit_code == STILL_ACTIVE)
+		{
+			CloseHandle(proc_info->hProcess);
+			CloseHandle(proc_info->hThread);
+
+			// Terminate child process forcfully
+			::TerminateProcess(proc_info->hProcess, 1);
+		}
+		delete proc_info;
+	}
+}
+
+worker& worker::operator=(const worker& other)
+{
+	return *this = worker(other);
+}
+
+worker& worker::operator=(worker&& other) noexcept
+{
+	std::swap(proc_info, other.proc_info);
+	return *this;
+}
